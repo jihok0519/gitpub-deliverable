3d75942de6d3441503de9d09698a9630eaed2168
Jiho Kim <jihok0519@gmail.com>
2022-05-17 16:45:46 -0400

    created show route for drink id

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.get('/drinks/', (req, res) => {
     });
 });
 
+app.get('/drinks/:id', (req, res) => {
+    res.send(req.params.id);
+});
+
 app.listen(port, () => {
     console.log("listening on port ", port);
 })
