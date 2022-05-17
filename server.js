b0a6fabc157ee615d9d6a57b5c947afaf8fc8927
Jiho Kim <jihok0519@gmail.com>
2022-05-17 17:09:16 -0400

    linked drink with drinks id

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ app.get('/drinks/', (req, res) => {
 });
 
 app.get('/drinks/:id', (req, res) => {
-    res.send(req.params.id);
+    res.render("drinks_show.ejs", {
+        drink: drinks[req.params.id],
+    });
 });
 
 app.listen(port, () => {
